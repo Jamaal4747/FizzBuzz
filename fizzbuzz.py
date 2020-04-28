e84fb5388540210349f609615e2c2d77b0f0150e
Jamaal Ridley <Jamaal@Jamaals-MacBook-Pro.local>
2020-04-28 19:14:57 -0400

    Created function for FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 def fizzbuzz(n):
-    if n % 5 == 0:
-        print("Buzz!")
+    if n % 5 == 0 & n % 3 == 0:
+        print("FizzBuzz!")
     elif n % 3 == 0:
         print("Fizz!")
+    elif n % 5 == 0:
+        print("Buzz!")
 
-fizzbuzz(25)
+fizzbuzz(30)
